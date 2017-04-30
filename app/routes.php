10a2ca28557d5e2e80094efb6b6aa0226fb89420
manoucheka <tutojeanbernard@mail.com>
2017-04-29 18:42:48 -0700

    second commit

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,11 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+//Route::get('/', function()
+//{
+//	return View::make('hello');
+//});
+
+Route::get('/', 'HomeController@index');
+
+Route::get('admin/index',array('as'=>'admin','uses'=>'HomeController@admin'));
